6f49b5fdb491b4cf0b1850001a3255eb5e6d0670
biweevil <jordan4rams@gmail.com>
2017-03-22 01:59:11 +0000

    remove capture

--- Main.js
+++ Main.js
@@ -7,7 +7,6 @@ CCGame.game.state.add('Boot', CCGame.Boot);
 //uncomment these as we create them through the tutorial
 CCGame.game.state.add('Preload', CCGame.Preload);
 CCGame.game.state.add('MainMenu', CCGame.MainMenu);
-CCGame.game.state.add('picCapture', CCGame.picCapture);
 CCGame.game.state.add('Game', CCGame.Game);
 CCGame.game.state.start('Boot');
 
